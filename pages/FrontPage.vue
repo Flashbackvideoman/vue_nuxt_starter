33f40981c1037aa8d9f000d1a3e7cd06908e8626
Flashbackvideoman <norm@strassner.com>
2024-03-29 14:27:27 -0600

    Added Tailwind, but not finished with it.

--- pages/FrontPage.vue
+++ pages/FrontPage.vue
@@ -1,13 +1,12 @@
 <template>
-    
-    <div id="FrontPage" class="full-height">
-        <div class="titleText">ASRCFederal Water Model Information Front Page</div>
-    </div>
-
+  <div id="FrontPage" class="full-height">
+    <div class="titleText">ASRCFederal Water Model Information Front Page</div>
+  </div>
 </template>
-<style type="css">
-html, body {
-  overflow:hidden;
+<style lang="scss">
+html,
+body {
+  overflow: hidden;
   height: 100%;
 }
 
@@ -15,14 +14,12 @@ html, body {
   height: 100%;
 }
 .titleText {
-    padding: 50px 0 0 100px;
-    font-size: 40px;
+  padding: 50px 0 0 100px;
+  font-size: 40px;
 }
 #FrontPage {
- 
-    height: 1024px;
-    color:white;
-    background: rgb(2,0,36);
-background: linear-gradient(130deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%);
+  height: 1024px;
+  color: white;
+
 }
 </style>
