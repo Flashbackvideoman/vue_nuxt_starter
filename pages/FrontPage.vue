ccb808e2d52b93491f7cf4231942faea0e2acd31
Flashbackvideoman <norm@strassner.com>
2024-03-29 09:24:12 -0600

    Created pages directory and enabled adding components

---
+++ pages/FrontPage.vue
@@ -0,0 +1,28 @@
+<template>
+    
+    <div id="FrontPage" class="full-height">
+        <div class="titleText">ASRCFederal Water Model Information Front Page</div>
+    </div>
+
+</template>
+<style type="css">
+html, body {
+  overflow:hidden;
+  height: 100%;
+}
+
+.full-height {
+  height: 100%;
+}
+.titleText {
+    padding: 50px 0 0 100px;
+    font-size: 40px;
+}
+#FrontPage {
+ 
+    height: 1024px;
+    color:white;
+    background: rgb(2,0,36);
+background: linear-gradient(130deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%);
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <Head>
+    <Title>ASRCFederal</Title>
+  </Head>
+  <div>
+    <FrontPage />
+  </div>
+</template>
+<script setup>
+import FrontPage from "./FrontPage.vue";
+
+onMounted(() => {
+  //this can be removed once we implement the page
+  // window.location.href = "/";
+});
+</script>
